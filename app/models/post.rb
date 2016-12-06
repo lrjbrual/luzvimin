eb6e7b2a9ee85467bc89570357cfe83f4277a490
Ryan Jeff <lrjbrual@lrjbrual.com>
2016-12-06 16:53:19 +0100

    backend configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :users
+
+  validates :title, presence: true
+  validates :category_id, presence: true
+  validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,100 +9,100 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   get 'posts/show'
-
-  namespace :admin do
-  get 'sessions/new'
-  end
-
-  namespace :admin do
-  get 'sessions/create'
-  end
-
-  namespace :admin do
-  get 'users/new'
-  end
-
-  namespace :admin do
-  get 'users/create'
-  end
-
-  namespace :admin do
-  get 'users/edit'
-  end
-
-  namespace :admin do
-  get 'users/update'
-  end
-
-  namespace :admin do
-  get 'users/destroy'
-  end
-
-  namespace :admin do
-  get 'users/index'
-  end
-
-  namespace :admin do
-  get 'users/show'
-  end
-
-  namespace :admin do
-  get 'categories/new'
-  end
-
-  namespace :admin do
-  get 'categories/create'
-  end
-
-  namespace :admin do
-  get 'categories/edit'
-  end
-
-  namespace :admin do
-  get 'categories/update'
-  end
-
-  namespace :admin do
-  get 'categories/destroy'
-  end
-
-  namespace :admin do
-  get 'categories/index'
-  end
-
-  namespace :admin do
-  get 'categories/show'
-  end
-
-  get 'categories/show'
-
-  namespace :admin do
-  get 'posts/new'
-  end
-
-  namespace :admin do
-  get 'posts/create'
-  end
-
-  namespace :admin do
-  get 'posts/edit'
-  end
-
-  namespace :admin do
-  get 'posts/update'
-  end
-
-  namespace :admin do
-  get 'posts/destroy'
-  end
-
-  namespace :admin do
-  get 'posts/index'
-  end
-
-  namespace :admin do
-  get 'posts/show'
-  end
+  #
+  # namespace :admin do
+  # get 'sessions/new'
+  # end
+  #
+  # namespace :admin do
+  # get 'sessions/create'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/new'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/create'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/edit'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/update'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/destroy'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/index'
+  # end
+  #
+  # namespace :admin do
+  # get 'users/show'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/new'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/create'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/edit'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/update'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/destroy'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/index'
+  # end
+  #
+  # namespace :admin do
+  # get 'categories/show'
+  # end
+  #
+  # get 'categories/show'
+  #
+  # namespace :admin do
+  # get 'posts/new'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/create'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/edit'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/update'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/destroy'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/index'
+  # end
+  #
+  # namespace :admin do
+  # get 'posts/show'
+  # end
 
   resources :posts, :categories
   resources 'contacts', only: [:new, :create]
